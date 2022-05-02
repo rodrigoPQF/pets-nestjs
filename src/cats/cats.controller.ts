00ffc42e4d6bc7cb6b390629ee62839a4c35c588
rodrigoPQF <rodrigopr404@gmail.com>
2022-05-02 16:28:12 -0300

    :construction: Cats local object

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -7,19 +7,26 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
+import { CatsModule } from './cats.module';
 @Controller('cats')
 export class CatsController {
+  cats: CatsModule[] = [
+    new CatsModule('Flinn', 12, 'Manicoon'),
+    new CatsModule('Blunaldo', 2, 'Manicoon'),
+    new CatsModule('Roneld', 4, 'Vira-lata'),
+  ];
   @Get(':id')
-  find(@Param() params): string {
-    return `This action return one cat ${params.id}`;
+  find(@Param() params): CatsModule {
+    return this.cats[params.id];
   }
   @Get()
-  findAll(): string {
-    return 'This action returns all cats';
+  findAll(): CatsModule[] {
+    return this.cats;
   }
 
   @Post()
   create(@Body() gato): string {
+    this.cats.push(gato);
     return `This action returns all cats if data ${JSON.stringify(gato)}`;
   }
 
@@ -30,6 +37,7 @@ export class CatsController {
 
   @Delete(':id')
   delete(@Param() params): string {
+    this.cats.pop();
     return `Apagar o gato ${params.id}`;
   }
 }
--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -2,8 +2,18 @@ import { Module } from '@nestjs/common';
 import { CatsController } from './cats.controller';
 import { CatsService } from './cats.service';
 
-@Module({
-  controllers: [CatsController],
-  providers: [CatsService],
-})
-export class CatsModule {}
+// @Module({
+//   controllers: [CatsController],
+//   providers: [CatsService],
+//   exports: [CatsService],
+// })
+export class CatsModule {
+  name: string;
+  age: number;
+  breed: string;
+  constructor(name: string, age: number, breed: string) {
+    this.name = name;
+    this.age = age;
+    this.breed = breed;
+  }
+}
